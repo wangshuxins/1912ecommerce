590d488104e1968235cbf18624df016bcbfe3415
cmwl <1357252390@qq.com>
2020-10-04 14:33:49 +0800

    列表bug修复

--- app/Http/Controllers/Index/SearchController.php
+++ app/Http/Controllers/Index/SearchController.php
@@ -94,10 +94,9 @@ class SearchController extends Controller
 
 		$p =  Request()->input('p');
 
-		if($p==1){
+		if($p){
 			$p = ($p-1);
 		}
-
 		$where = [];
 
 		if(!empty($brand_id)){
@@ -122,7 +121,7 @@ class SearchController extends Controller
 			 $limit = GoodsModel::where($where)->whereIn('cate_id',$cate_ids)->whereBetween('goods_price',[$str_replace[0],$str_replace[1]])->offset($p)->limit($page_num)->get();
 			 $count = GoodsModel::where($where)->whereIn('cate_id',$cate_ids)->whereBetween('goods_price',[$str_replace[0],$str_replace[1]])->count();
 		 }else{
-			 $limit = GoodsModel::where($where)->whereIn('cate_id',$cate_ids)->offset($p)->limit($page_num)->get();
+			 $limit = GoodsModel::where($where)->whereIn('cate_id',$cate_ids)->offset($p*10)->limit(10)->get();
 			 $count = GoodsModel::where($where)->whereIn('cate_id',$cate_ids)->count();
 		 }
 		$page_count = ceil($count/$page_num);
